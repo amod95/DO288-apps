bd63db65a69537297077af9fab2c5d3786b04e2a
Student User <student@workstation.lab.example.com>
2020-03-19 15:07:37 +0000

    Changes to nodejs directory

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Supppppppp\n');
+  res.send('Hello My Friend\n');
 });
 
 app.listen(8080, function () {
